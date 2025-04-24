2ca87afe24ede00981223ce78a0e35bf6620e2ce
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2025-04-24 08:28:19 -0400

    add colors theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color.fromRGBO(222, 222, 222, 1);
+  static const sideNav = Color.fromRGBO(222, 222, 222, 1);
+  static const searchBar = Color.fromRGBO(255, 255, 255, 1);
+  static const searchBarBorder = Color.fromRGBO(212, 212, 212, 1);
+  static const iconGrey = Color(0xFF909090);
+  static const textGrey = Color(0xFFAAAAAA);
+  static const footerGrey = Color(0xFF737373);
+  static const proButton = Color.fromRGBO(62, 62, 62, 1);
+  static const cardColor = Color.fromARGB(255, 81, 81, 81);
+  static const submitButton = Color.fromRGBO(27, 185, 206, 1);
+}
