0c1a18fada61829b2fbaeb8e43e20865ea1fb08f
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2025-05-12 14:03:39 -0400

    update theme

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const background = Color.fromRGBO(222, 222, 222, 1);
-  static const sideNav = Color.fromRGBO(222, 222, 222, 1);
+  static const sideNav = Color.fromRGBO(214, 214, 214, 1);
   static const searchBar = Color.fromRGBO(255, 255, 255, 1);
   static const searchBarBorder = Color.fromRGBO(212, 212, 212, 1);
   static const iconGrey = Color(0xFF909090);
@@ -11,4 +11,6 @@ class AppColors {
   static const proButton = Color.fromRGBO(62, 62, 62, 1);
   static const cardColor = Color.fromARGB(255, 81, 81, 81);
   static const submitButton = Color.fromRGBO(27, 185, 206, 1);
+  static const whiteColor = Color.fromRGBO(255, 255, 255, 1);
+  
 }
